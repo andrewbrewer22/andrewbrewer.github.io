9bc31599da3ef02ac4353e2e9496befd0fe5536c
Andrew Brewer <72884247+andrewbrewer22@users.noreply.github.com>
2022-02-27 20:43:33 -0600

    first commit

---
+++ react-resume-exhibit/resume-exhibit/src/index.js
@@ -0,0 +1,140 @@
+
+import React from "react";
+import ReactDOM from 'react-dom';
+import './index.css';
+
+class NavBar extends React.Component {
+    render() {
+        return (
+
+            <body class="body">
+
+                <section class="break-container">
+
+                    <section class="leftbreak">
+
+                        <div class="contact-info">
+                            <span class="br" id="bold-this">Full-Stack Developer</span>
+                            <span class="br">Andrew Brewer</span>
+                            <span class="br">253-244-8669</span>
+                            <span class="br">andrewwilsonbrewer22@gmail.com</span>
+
+
+                            <hr class="divider-line"></hr>
+                        </div>
+
+                        <div class="resources">
+                            <span class="br">Programming Languages:</span>
+                            <span class="br" id="bold-this"> HTML, CSS, JS, C#</span>
+                            <span class="br">Frameworks and Libraries:</span>
+                            <span class="br" id="bold-this">Angular, React, Node.JS, ASP.NET Core/MVC</span>
+                            <span class="br">DataBase Software:</span>
+                            <span class="br" id="bold-this">MySQL</span>
+
+                            <hr class="divider-line"></hr>
+                        </div>
+
+                        <div class="my-links">
+                            <a class="br" href="../BMIcalc/bmi-index.html">My Links</a>
+                            <a class="br" href="../BMIcalc/bmi-index.html">Link Here</a>
+                            <a class="br" href="../BMIcalc/bmi-index.html">Link Here</a>
+                            <a class="br" href="../BMIcalc/bmi-index.html">Link Here</a>
+
+                            <hr class="divider-line"></hr>
+                        </div>
+
+                        <div class="my-projects">
+                            <a class="br" href="../BMIcalc/bmi-index.html">My Projects</a>
+                            <a class="br" href="../BMIcalc/bmi-index.html">BMI Calculator</a>
+                            <a class="br" href="../BMIcalc/bmi-index.html">Link Here</a>
+                            <a class="br" href="../BMIcalc/bmi-index.html">Link Here</a>
+
+                            <hr class="divider-line"></hr>
+                        </div>
+                    </section>
+
+                    <section class="rightbreak">
+                        <div class="img">
+                            <img class="self-portrait" src={require('./images/Andrew_Brewer.jpg')} alt="Andrew_Brewer"></img>
+                        </div>
+
+
+                        <div class="basic-information">
+                            <p>
+                                Put some code here for the sake of filling up space on the screen.
+                                Put some code here for the sake of filling up space on the screen.
+                                Put some code here for the sake of filling up space on the screen.
+                                Put some code here for the sake of filling up space on the screen.
+                                Put some code here for the sake of filling up space on the screen.
+                                Put some code here for the sake of filling up space on the screen.
+                                Put some code here for the sake of filling up space on the screen.
+                                Put some code here for the sake of filling up space on the screen.
+                            </p>
+                        </div>
+
+                        <div class="my-skills">
+                            <p>
+                                Put some code here for the sake of filling up space on the screen.
+                                Put some code here for the sake of filling up space on the screen.
+                                Put some code here for the sake of filling up space on the screen.
+                                Put some code here for the sake of filling up space on the screen.
+                                Put some code here for the sake of filling up space on the screen.
+                                Put some code here for the sake of filling up space on the screen.
+                                Put some code here for the sake of filling up space on the screen.
+                                Put some code here for the sake of filling up space on the screen.
+                            </p>
+                        </div>
+
+                        <div class="my-talents">
+                            <p>
+                                Put some code here for the sake of filling up space on the screen.
+                                Put some code here for the sake of filling up space on the screen.
+                                Put some code here for the sake of filling up space on the screen.
+                                Put some code here for the sake of filling up space on the screen.
+                                Put some code here for the sake of filling up space on the screen.
+                                Put some code here for the sake of filling up space on the screen.
+                                Put some code here for the sake of filling up space on the screen.
+                                Put some code here for the sake of filling up space on the screen.
+                            </p>
+                        </div>
+
+                        <div class="my-education">
+                            <p>
+                                Put some code here for the sake of filling up space on the screen.
+                                Put some code here for the sake of filling up space on the screen.
+                                Put some code here for the sake of filling up space on the screen.
+                                Put some code here for the sake of filling up space on the screen.
+                                Put some code here for the sake of filling up space on the screen.
+                                Put some code here for the sake of filling up space on the screen.
+                                Put some code here for the sake of filling up space on the screen.
+                                Put some code here for the sake of filling up space on the screen.
+                            </p>
+                        </div>
+
+                        <div class="achievements">
+                            <p>
+                                Put some code here for the sake of filling up space on the screen.
+                                Put some code here for the sake of filling up space on the screen.
+                                Put some code here for the sake of filling up space on the screen.
+                                Put some code here for the sake of filling up space on the screen.
+                                Put some code here for the sake of filling up space on the screen.
+                                Put some code here for the sake of filling up space on the screen.
+                                Put some code here for the sake of filling up space on the screen.
+                                Put some code here for the sake of filling up space on the screen.
+                            </p>
+                        </div>
+
+
+                        <div>
+
+                        </div>
+
+                    </section>
+                </section>
+            </body>
+
+        );
+    }
+}
+
+ReactDOM.render(<NavBar />, document.getElementById('root'))
