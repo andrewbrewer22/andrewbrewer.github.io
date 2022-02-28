d36f482f12d4bc07aef4a74bc3898ce76ebadd96
Andrew Brewer <72884247+andrewbrewer22@users.noreply.github.com>
2022-02-27 21:01:15 -0600

    added background color

--- react-resume-exhibit/resume-exhibit/src/index.js
+++ react-resume-exhibit/resume-exhibit/src/index.js
@@ -5,6 +5,7 @@ import './index.css';
 
 class NavBar extends React.Component {
     render() {
+        document.body.style = 'background: rgb(65, 65, 65);';
         return (
 
             <body class="body">
