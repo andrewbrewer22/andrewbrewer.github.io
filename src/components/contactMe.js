13a488a903af6a36a277537c0844d8fe1eb7a6e3
Andrew Brewer <72884247+andrewbrewer22@users.noreply.github.com>
2022-04-08 22:34:05 -0500

    added decent modal and working buttons

--- src/components/contactMe.js
+++ src/components/contactMe.js
@@ -9,36 +9,11 @@ import "../styles/modal.css";
 export default function ContactMe() {
     const [contactIsOpen, setContactIsOpen] = useState(false);
     return (
-        <main>
-            <button className={"primaryBtn"} onClick={() => setContactIsOpen(true)}>
+        <main className="primaryBtn-container">
+            <button className="primaryBtn" onClick={() => setContactIsOpen(true)}>
                 Contact Me
             </button>
             {contactIsOpen && <ContactModal setContactIsOpen={setContactIsOpen} />}
         </main>
-            
-            
-        /*
-        <form className="contactme">
-            
-            <span className="br" id="bold-this">Contact Me</span>
-            Name
-            <div>
-                <input className="contact-personal" id="name" placeholder="John Doe"></input>
-            </div>
-            Email
-            <div>
-                <input className="contact-personal" id="email" placeholder="youremail@email.com"></input>
-            </div>
-            Message or Inquiry
-            <div>
-                <textarea type="text" className="contact-message" id="message" placeholder="Say Hello!"></textarea>
-            </div>
-            <div>
-                <button type="submit" className="contact-submit" id="contact-submit">
-                    Submit
-                </button>
-            </div>
-        </form>
-        */
     );
 }
--- src/components/contactModal.js
+++ src/components/contactModal.js
@@ -1,8 +1,10 @@
 import React from "react"
 import "../styles/modal.css";
+import { RiCloseLine } from "react-icons/ri";
 
 const ContactModal = ({ setContactIsOpen }) => {
     return (
+        disableScroll(),
         <main className="contact-body">
             <div className="contact-content">
                 <form className="contact-form">
@@ -19,12 +21,61 @@ const ContactModal = ({ setContactIsOpen }) => {
                         <textarea type="text" className="contact-message" id="message" placeholder="Say Hello!"></textarea>
                     </span>
 
-                    <button className="contact-submit" onClick={() => setContactIsOpen(false)}>
+                    <button className="contact-submit" onClick={() => {setContactIsOpen(false); enableScroll();}}>
                         Submit
+                    </button>
+
+                    <button className="contact-exit" onClick={() => {setContactIsOpen(false); enableScroll();}}>
+                        <RiCloseLine style={{ marginBottom: "-3px" }} />
                     </button>
                 </form>
             </div>
+
         </main>
     );
 }
 export default ContactModal;
+
+
+//#region no scrolling courtesy of https://stackoverflow.com/questions/4770025/how-to-disable-scrolling-temporarily
+var keys = { 37: 1, 38: 1, 39: 1, 40: 1 };
+
+function preventDefault(e) {
+    e.preventDefault();
+}
+
+function preventDefaultForScrollKeys(e) {
+    if (keys[e.keyCode]) {
+        preventDefault(e);
+        return false;
+    }
+}
+
+// modern Chrome requires { passive: false } when adding event
+var supportsPassive = false;
+try {
+    window.addEventListener("test", null, Object.defineProperty({}, 'passive', {
+        get: function () { supportsPassive = true; }
+    }));
+} catch (e) { }
+
+var wheelOpt = supportsPassive ? { passive: false } : false;
+var wheelEvent = 'onwheel' in document.createElement('div') ? 'wheel' : 'mousewheel';
+
+// call this to Disable
+function disableScroll() {
+    window.addEventListener('DOMMouseScroll', preventDefault, false); // older FF
+    window.addEventListener(wheelEvent, preventDefault, wheelOpt); // modern desktop
+    window.addEventListener('touchmove', preventDefault, wheelOpt); // mobile
+    window.addEventListener('keydown', preventDefaultForScrollKeys, false);
+}
+
+// call this to Enable
+function enableScroll() {
+    window.removeEventListener('DOMMouseScroll', preventDefault, false);
+    window.removeEventListener(wheelEvent, preventDefault, wheelOpt);
+    window.removeEventListener('touchmove', preventDefault, wheelOpt);
+    window.removeEventListener('keydown', preventDefaultForScrollKeys, false);
+}
+
+//#endregion
