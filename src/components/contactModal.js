afb539548678c1dbaa43e8c0b0e44367dee70601
Andrew Brewer <72884247+andrewbrewer22@users.noreply.github.com>
2022-04-16 00:28:28 -0500

    removed network code

--- src/components/contactModal.js
+++ src/components/contactModal.js
@@ -6,27 +6,27 @@ import axios from "axios";
 const ContactModal = ({ setContactIsOpen }) => {
 
     //#region handle contact form submissions
-    const [status, setStatus] = useState("Submit");
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        setStatus("Sending...");
-        const { name, email, message } = e.target.elements;
-        let details = {
-            name: name.value,
-            email: email.value,
-            message: message.value,
-        };
-        let response = await fetch("http://localhost:5000/contact", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json;charset=utf-8",
-            },
-            body: JSON.stringify(details),
-        });
-        setStatus("Submit");
-        let result = await response.json();
-        alert(result.status);
-    };
+    // const [status, setStatus] = useState("Submit");
+    // const handleSubmit = async (e) => {
+    //     e.preventDefault();
+    //     setStatus("Sending...");
+    //     const { name, email, message } = e.target.elements;
+    //     let details = {
+    //         name: name.value,
+    //         email: email.value,
+    //         message: message.value,
+    //     };
+    //     let response = await fetch("http://localhost:5000/contact", {
+    //         method: "POST",
+    //         headers: {
+    //             "Content-Type": "application/json;charset=utf-8",
+    //         },
+    //         body: JSON.stringify(details),
+    //     });
+    //     setStatus("Submit");
+    //     let result = await response.json();
+    //     alert(result.status);
+    // };
     //#endregion
 
     return (
