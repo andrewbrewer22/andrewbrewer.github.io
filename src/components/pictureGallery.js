e546e49029e1a4c103cbafeaa79800497432181d
Andrew Brewer <72884247+andrewbrewer22@users.noreply.github.com>
2022-05-15 19:05:54 -0700

    added images

--- src/components/pictureGallery.js
+++ src/components/pictureGallery.js
@@ -1,17 +1,15 @@
 import React from "react";
 import "../styles/mainStyle.css";
 
-export default class PictureGalleryComponent extends React.Component{
-    render(){
-        return(
+export default class PictureGalleryComponent extends React.Component {
+    render() {
+        return (
             <section class="rightbreak">
                 <div className="picture-gallery-title">
                     <p>
                         Picture Gallery
                     </p>
                 </div>
-
-
             </section>
         );
     }
