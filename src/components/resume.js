3fbc894c72b873d1f027c7944e71bee437992816
Andrew Brewer <72884247+andrewbrewer22@users.noreply.github.com>
2022-05-17 19:21:24 -0500

    changed home page

--- src/components/resume.js
+++ src/components/resume.js
@@ -1,10 +1,15 @@
 import React from "react";
 import "../styles/resume.css";
+import "../fonts/MKBritishWriting.ttf";
+import "../styles/fonts.css";
 
 export default class Resume extends React.Component {
   render() {
     return (
       <section class="rightbreak">
+        <div>
+        <span className="resume-title" id="bold-this">Andrew Brewer</span>
+        </div>
         <div>
           <img
             class="self-portrait"
@@ -15,14 +20,7 @@ export default class Resume extends React.Component {
 
         <div class="resume-text">
           <p>
-            Put some code here for the sake of filling up space on the screen.
-            Put some code here for the sake of filling up space on the screen.
-            Put some code here for the sake of filling up space on the screen.
-            Put some code here for the sake of filling up space on the screen.
-            Put some code here for the sake of filling up space on the screen.
-            Put some code here for the sake of filling up space on the screen.
-            Put some code here for the sake of filling up space on the screen.
-            Put some code here for the sake of filling up space on the screen.
+            Thank you for taking the time to check out my website!
           </p>
         </div>
 
