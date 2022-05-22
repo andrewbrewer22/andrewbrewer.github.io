ab690e26edd4d4b035dfafd16249a0d7a23179d8
Andrew Brewer <72884247+andrewbrewer22@users.noreply.github.com>
2022-05-21 18:12:51 -0700

    fixed grammer

--- src/components/resume.js
+++ src/components/resume.js
@@ -25,7 +25,7 @@ export default class Resume extends React.Component {
             Thank you for taking the time to check out my website! I created
             this React app from scratch to demonstrate my competency and
             efficiency in coding. I am currently working as a Special Education
-            Pre-K assistant in the Frenship Independent School Distract.
+            Pre-K assistant in the Frenship Independent School District.
           </p>
         </div>
 
@@ -39,13 +39,13 @@ export default class Resume extends React.Component {
 
         <div class="resume-text">
           <p>
-            Through the Austin Coding Academy I learned the major parts of
-            developing a website. Initially <span id="bold-this">github</span>{" "}
+            Through the Austin Coding Academy, I learned the major parts of
+            developing a website. Initially, <span id="bold-this">GitHub</span>{" "}
             was the class topic, a strong emphasis on{" "}
             <span id="bold-this">version control</span> was prominent. We
             learned <span id="bold-this">HTML</span>,{" "}
             <span id="bold-this">CSS</span>, and{" "}
-            <span id="bold-this">Java Script</span> so that we could create{" "}
+            <span id="bold-this">JavaScript</span> so that we could create{" "}
             <span id="bold-this">Front-End</span> sites and generally learn how
             programming languages worked together. When learning the{" "}
             <span id="bold-this">Back-End</span> side of development we used{" "}
@@ -56,7 +56,7 @@ export default class Resume extends React.Component {
             <span id="bold-this">jQuery</span> to learn how a server
             communicates <span id="bold-this">data </span>
             to other servers and the front-end. As well as{" "}
-            <span id="bold-this">securing and interpreting</span> said data
+            <span id="bold-this">securing and interpreting</span> said data.
           </p>
         </div>
 
@@ -70,7 +70,7 @@ export default class Resume extends React.Component {
 
         <div class="resume-text">
           <p>
-            During my service in the <span id="bold-this">U.S. Army</span> I
+            During my service in the <span id="bold-this">U.S. Army,</span> I
             trained as a{" "}
             <span id="bold-this">Joint Fire Support Specialist</span> (MOS:
             13F). My primary responsibility was to gain{" "}
@@ -80,20 +80,18 @@ export default class Resume extends React.Component {
               identification, location, and the correct munitions
             </span>{" "}
             (60mm, 81mm, 120mm mortar systems, 105mm and 155mm artillery
-            systems, aircraft and naval batteries) to incapacitate or destroy
+            systems, aircraft, and naval batteries) to incapacitate or destroy
             the target in{" "}
-            <span id="bold-this">
-              support of the advancement of friendly troops. 
-            </span>
+            <span id="bold-this">support of the advancement of friendly troops. </span>
             My secondary responsibility was to{" "}
-            <span id="bold-this">maintain</span> radio and gps/satellite{" "}
+            <span id="bold-this">maintain</span> radio and GPS/satellite{" "}
             <span id="bold-this">equipment</span>, and current location and
-            positions of allied troops, and of course all other soldier tasks
+            positions of allied troops, and all other soldier tasks
             that are required. Another important task was constant{" "}
-            <span id="bold-this">communications with our leadership </span>
+            <span id="bold-this">communication with our leadership </span>
             regardless of rank or position. This meant that we were to give{" "}
-            <span id="bold-this">briefs</span>, or other important information
-            to higher ranking officers and NonCommissioned officers in a{" "}
+            <span id="bold-this">briefs</span> or other important information
+            to higher-ranking officers and NonCommissioned officers in a{" "}
             <span id="bold-this">precise and coherent</span> manner.
           </p>
         </div>
